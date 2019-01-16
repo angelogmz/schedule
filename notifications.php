0ba9e8ba2138c1d2f5550a8242f57bf940203273
Angelo <angelo@antyrasolutions.com>
2019-01-16 19:01:01 +0530

    top section

---
+++ notifications.php
@@ -0,0 +1,50 @@
+
+ <!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600" rel="stylesheet"> 
+<link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" rel="stylesheet"> 
+<link href="styles.min.css" rel="stylesheet"> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<title>Schedule</title>
+</head>
+
+<body>
+<div class="dashboard-body">
+    <div class="dashboard-upper">
+        <div class="dashboard-top-left">
+            <img class="bg-img" src="images/sign-in-logo.jpg" alt="sign-in" />
+        </div>
+        <div class="dashboard-top-right">
+            <div class="search-bar">
+                <input id="mysearch2" type="search" placeholder="search">
+            </div>
+            <div class="info-bar">
+                <div class="notification-icons bell">
+                    <img class="not-img" src="images/bell.svg" alt="notification" />
+                </div>
+                <div class="notification-icons bell">
+                    <img class="not-img" src="images/message.svg" alt="message" />
+                </div>
+                <div class="profile-btn">
+                    <div class="pro-img">
+                        <img class="not-img" src="images/profile.jpg" alt="profile" />
+                    </div>
+                    <div class="profile-info">
+                        <span class="pro-name">John Doe</span>
+                        <span class="pro-desig">Project Manager</span>
+                    </div>
+                    <div class="more-btn">
+                        <img class="not-img" src="images/down-arrow.svg" alt="notification" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+</body>
+
+</html> 
---
+++ password-reset.php
@@ -0,0 +1,29 @@
+
+ <!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600" rel="stylesheet"> 
+<link href="styles.min.css" rel="stylesheet"> 
+<title>Schedule</title>
+</head>
+
+<body>
+<div class="sign-in-body" style="background: url(images/sign-in-bg.jpg) no-repeat 50% 50% /cover">
+    <div class="sign-in-form">
+        <div class="form-logo">
+            <img class="bg-img" src="images/sign-in-logo.jpg" alt="sign-in" />
+            <div class="logo-intro">Reset your password</div>
+        </div>
+        <form class="sign-form">
+            <input id="pw-reset-email" name="pw-reset-email" type="email" placeholder="Email Address">
+            <input class="btn-submit" type="submit" value="Send Password Reset">
+        </form>
+        <div class="form-bottom">
+            Already have an account ? <a href="/index.php">Sign in</a>
+        </div>
+    </div>
+</div>
+</body>
+
+</html> 
